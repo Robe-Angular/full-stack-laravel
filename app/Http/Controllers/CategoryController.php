8964b171a50c71c11c563ac6a484624f57d8e836
robe <robe@robe.com>
2022-11-28 21:51:02 -0600

    normal cors on index.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,6 +11,7 @@ class CategoryController extends Controller {
 
     public function __construct() {
         $this->middleware('api.auth', ['except' => ['index', 'show']]);
+        
     }
 
     public function index() {
--- routes/web.php
+++ routes/web.php
@@ -52,31 +52,31 @@ Route::get('/testOrm', 'PruebasController@testOrm');
     Route::get('/categoria/pruebas','CategoryController@pruebas');
     Route::get('/entrada/pruebas','PostController@pruebas');
   */  
-    Route::group(['middleware' => ['cors']], function () {
-    //Rutas del controlador de usuario
-    //Route::post('/api/register', 'UserController@register');
-    Route::post('/api/login', 'UserController@login');
-    //Route::put('/api/user/update', 'UserController@update');
-    //Route::post('/api/user/upload','UserController@upload')->middleware(ApiAuthMiddleware::class);
-    //Route::get('/api/user/avatar/{filename}', 'UserController@getImage');
-    //Route::get('/api/user/detail/{id}', 'UserController@detail');
-    
-    //Rutas del controlador categorías
-    Route::resource('/api/category', 'CategoryController');
-    
-    
-    //Rutas del controlador entradas
-    Route::resource('/api/post', 'PostController');
-    Route::post('/api/post/upload','PostController@upload');
-    Route::get('/api/post/category/{id}', 'PostController@getPostsByCategory');
-    Route::get('/api/post/user/{id}', 'PostController@getPostsByUser');
-    Route::get('/api/list-post-admin', 'PostController@getPostsInAdmin');
-    Route::get('/api/post/publish/{id}/{value}', 'PostController@setPublished');
-    
-    //imageController
-    Route::resource('/api/image', 'ImageController');
-    Route::get('/api/post/image/{file_description}', 'ImageController@getImage');
-    Route::post('/api/image/save', 'ImageController@saveImage');
-    
-    Route::get('api/images-by-post/{post_id}','ImageController@getImagesByPost');
-    });
+   //Route::group(['middleware' => ['cors']], function () {
+        //Rutas del controlador de usuario
+        //Route::post('/api/register', 'UserController@register');
+        Route::post('/api/login', 'UserController@login');
+        //Route::put('/api/user/update', 'UserController@update');
+        //Route::post('/api/user/upload','UserController@upload')->middleware(ApiAuthMiddleware::class);
+        //Route::get('/api/user/avatar/{filename}', 'UserController@getImage');
+        //Route::get('/api/user/detail/{id}', 'UserController@detail');
+
+        //Rutas del controlador categorías
+        Route::resource('/api/category', 'CategoryController');
+
+
+        //Rutas del controlador entradas
+        Route::resource('/api/post', 'PostController');
+        Route::post('/api/post/upload','PostController@upload');
+        Route::get('/api/post/category/{id}', 'PostController@getPostsByCategory');
+        Route::get('/api/post/user/{id}', 'PostController@getPostsByUser');
+        Route::get('/api/list-post-admin', 'PostController@getPostsInAdmin');
+        Route::get('/api/post/publish/{id}/{value}', 'PostController@setPublished');
+
+        //imageController
+        Route::resource('/api/image', 'ImageController');
+        Route::get('/api/post/image/{file_description}', 'ImageController@getImage');
+        Route::post('/api/image/save', 'ImageController@saveImage');
+
+        Route::get('api/images-by-post/{post_id}','ImageController@getImagesByPost');
+    //});
