25e1f7de9fcb0515826ae1a8a7c0da2ddc5feaf5
robe <robe@robe.com>
2024-01-31 18:37:57 -0600

    migration change column_name name to description on categories

--- app/Category.php
+++ app/Category.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $table = 'categories';
-    
+
     //Relación de uno a muchos
     public function posts(){
         return $this->hasMany('App\Post');
     }
-    
+
     public function categories_language(){
         return $this->hasMany('App\Categories_Language');
     }
